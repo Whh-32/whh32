8c258a045fa6650a091b6a723b75137959692f49
AmirHossein Lashkari <jdoe@example.com>
2024-10-08 01:11:37 +0330

    update

--- src/components/header/hero.tsx
+++ src/components/header/hero.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 import { Button } from "@/components/ui/button";
 import { ConfettiSideCannons } from "@/components/header/buttonConfetti";
 import BlurFade from "@/components/ui/blur-fade";
-import RiveComp from "@/components/rive/riveComp";
+import Skully from "@/components/rive/skully";
 
 function Hero() {
     return (
         <div className="w-full max-w-6xl mx-auto px-5 relative md:flex">
             <div className='fcc'>
-                <RiveComp />
+                <Skully />
             </div>
             <BlurFade delay={0.25} inView className="flex flex-col items-center md:items-start">
                 <h1 className="mt-6 w-full font-heading text-4xl font-normal leading-[1.25] drop-shadow-md sm:mt-8 sm:text-5xl sm:leading-[1.15] lg:text-5xl lg:leading-[1.15] xl:text-6xl xl:leading-[1.15] mb-4 text-center md:text-justify flex flex-col md:mt-0">
---
+++ src/components/rive/skully.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useRive } from "@rive-app/react-canvas";
+import { ReactElement } from "react";
+// import riveWasmUrl from "@rive-app/canvas/rive.wasm";
+
+// Set the WASM URL
+// RuntimeLoader.setWasmUrl(riveWasmUrl);
+
+export const RiveDemo = () => {
+    const { rive, RiveComponent } = useRive({
+        src: "skully.riv",
+        stateMachines: "State Machine [Skully]",
+        autoplay: true,
+    });
+
+    return <RiveComponent className="h-full w-full" />;
+};
+
+export default function Skully() {
+    return (
+        <div className="h-[400px] w-[400px] z-10 md:absolute right-0 mt-20 md:mt-auto">
+            <RiveDemo />
+        </div>
+    );
+}
