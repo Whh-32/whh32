2c39e14f2625100b7cd97008f8e0be9f2e6fe9dc
Your Name <am.lashkari2017@gmail.com>
2026-02-28 04:12:51 +0330

    fix scroll bar

--- src/components/rive/skully.tsx
+++ src/components/rive/skully.tsx
@@ -21,27 +21,27 @@ export const RiveDemo = () => {
 export default function Skully() {
     const skullyRef = useRef(null)
 
-    useGSAP(() => {
-        const element = document.querySelector<HTMLElement>('.skully');
-        const elementWidth = element?.offsetWidth;
-        const elementHeight = element?.offsetHeight;
+    // useGSAP(() => {
+    //     const element = document.querySelector<HTMLElement>('.skully');
+    //     const elementWidth = element?.offsetWidth;
+    //     const elementHeight = element?.offsetHeight;
 
-        const tl1 = gsap.timeline({
-            scrollTrigger: {
-                trigger: ".hero",
-                start: 'top top',
-                end: '+=200',
-                scrub: 2,
-                pin: true,
-            }
-        })
+    //     const tl1 = gsap.timeline({
+    //         scrollTrigger: {
+    //             trigger: ".hero",
+    //             start: 'top top',
+    //             end: '+=200',
+    //             scrub: 2,
+    //             pin: true,
+    //         }
+    //     })
 
-        // tl1.from(skullyRef.current, {
-        //     right: "calc(50% - 200px)",
-        //     top: (window.innerHeight - elementHeight!) / 2
-        // });
+    //     // tl1.from(skullyRef.current, {
+    //     //     right: "calc(50% - 200px)",
+    //     //     top: (window.innerHeight - elementHeight!) / 2
+    //     // });
 
-    }, []);
+    // }, []);
 
     return (
         <div ref={skullyRef} className="h-[250px] w-[250px] sm:h-[300px] sm:w-[300px] md:h-[400px] md:w-[400px] z-10 skully flex-shrink-0">
